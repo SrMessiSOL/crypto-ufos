859511573efef64bf0dd1447e3c210169ae7513c
SrMessi <srmessisol@gmail.com>
2025-09-20 21:07:19 -0300

    Update page.tsx

--- app/GORPVP/page.tsx
+++ app/GORPVP/page.tsx
@@ -47,16 +47,6 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -1238,5 +1228,6 @@ const WrappedBettingGame = () => {
     </ConnectionProvider>
   );
 };
+
 
 export default WrappedBettingGame;
